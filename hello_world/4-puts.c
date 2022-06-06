95d2f0373eef21b8f69ac605ecdfac97dba822ea
Raulj06 <5015@holbertonschool.com>
2022-06-06 15:00:46 +0000

    hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main ()
-{
-   puts ("Programming is like building a multilingual puzzle");
-   
-      return 0;
-}   
+puts "((Programming is like building a multilingual puzzle))"  
