dc81b641ebcfb3fb907c2f7ca4f48fd29d96a7e5
Raulj06 <5015@holbertonschool.com>
2022-06-06 18:05:23 +0000

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char:4
+        int:4
+	float:4
+	long int:4
+	double:4
+	
+	
+	printf("Size of char pointer: %d\n"     ,sizeof(char*));
+	printf("\nSize of int pointer: %d\n"      ,sizeof(int*));
+        printf("\nSize of float pointer: %d\n"    ,sizeof(float*));
+        printf("\nSize of long int pointer: %d\n" ,sizeof(long int*));
+        printf("\nSize of double pointer: %d\n" ,sizeof(double*));
+	return (0);
+}
