6ae98890f37e719335f3124a08888ede9f0ac07c
Raulj06 <5015@holbertonschool.com>
2022-06-06 16:49:04 +0000

    hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 
+/*
+ * prints exacly "Programming is like building a multilingual puzzle"
+ * should end with the value 0
+ * use the function puts
+ */
+
 int main (void)
 
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts ("Programming is like building a multilingual puzzle\n");
 	 
 	return 0;
 } 
