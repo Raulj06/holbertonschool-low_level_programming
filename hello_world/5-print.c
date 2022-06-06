527b10a62aded656c12d8dfcff41396e4e29d183
Raulj06 <5015@holbertonschool.com>
2022-06-06 17:28:28 +0000

    hello, printf

---
+++ hello_world/5-print.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 0
+ */
+
+int main()
+{
+      printf("with proper grammar, but the outcome is a piece of art,");
+
+       return (0);
+}      
