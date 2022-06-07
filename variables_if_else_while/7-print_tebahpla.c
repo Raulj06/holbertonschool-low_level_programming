dcd746523ee1b34383aab442e289672132ffb5a7
Raulj06 <5015@holbertonschool.com>
2022-06-07 22:54:29 +0000

    print numbers in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - lowercase alphabet in reverse
+ * Return: 0
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z' ; n >= 'a' ; --n)
+
+	putchar(n);
+
+	puntchar('\n');
+
+	return (0);
+}
