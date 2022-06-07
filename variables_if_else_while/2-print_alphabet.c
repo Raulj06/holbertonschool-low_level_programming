185c98f93cf501fce50d7d1e4a0db6e477e2bb30
Raulj06 <5015@holbertonschool.com>
2022-06-07 21:07:48 +0000

    alphabet in lower

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,7 @@ int main(void)
 	char low;
 
 
-	for (low x = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);	     
 	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
