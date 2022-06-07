0fbcdc6af8050702466f6208b76dddcb2f16f32b
Raulj06 <5015@holbertonschool.com>
2022-06-07 21:29:40 +0000

    alphabet in lower

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 /**
- * main - alphabet lowercase
- * 
- * Return: 0
- */
+* main - alphabet lowercase
+*
+* Return: 0
+*/
 
 int main(void)
 {
 	char letter;
 
-	for(letter = 'a' ; letter <= 'z' ; letter++);
+	for (letter = 'a' ; letter <= 'z' ; letter++)
 	putchar(letter);
-	
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
