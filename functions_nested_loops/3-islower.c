6961bde4f71a1a2d8f3c77fa226f01ee7b734db7
Raulj06 <5015@holbertonschool.com>
2022-06-08 22:04:18 +0000

    checks for lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * int _islower(int c) - checks for lowercase
+ *@c: letter test
+ * Return: Always 0.
+ */
+
+int _islower(int c);
+{
+
+	for (c > a && c < z)
+	{
+
+		return (1);
+	}
+
+	else
+
+		return (0);
+}
