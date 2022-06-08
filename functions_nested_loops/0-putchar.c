bcbc8514e49ecd0c88373fcfb8a87c01d34a9428
Raulj06 <5015@holbertonschool.com>
2022-06-08 14:39:23 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<holberton.h>
+#include"_putchar"
 
 /**
  * main - prints putchar
@@ -7,16 +7,15 @@
 
 int main(void)
 {
-	_putchar('h');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	
-	_putchar('\n')
 	return (0);
 }
