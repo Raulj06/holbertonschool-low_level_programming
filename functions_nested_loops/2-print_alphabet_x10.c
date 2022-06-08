e25648fdc71d2ce4426d4c4a2944a2449007cb7b
Raulj06 <5015@holbertonschool.com>
2022-06-08 21:46:08 +0000

    print 10 times alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include"main.h"
 /**
- * main - prints 10 times the alphabet
+ * main - prints 10 times the alphabet in lowercase
  *
  * Return: 0
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 
 	char lowercase;
