a8e0aa9d7ad53fb3a3493dbc1fd7edb596b30a38
Raulj06 <5015@holbertonschool.com>
2022-06-08 15:23:31 +0000

    insomnia

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"1-main.c"
+/**
+ * main - print alphabet in lowercase
+ * Return: 0
+*/
+
+int main(void);
+{
+	char n;
+
+	void print_alphabet(void);
+
+	for (n = 'a' ; n = 'z'; n++)
+
+{
+	_putchar(n);
+
+	_putchar('\n');
+}
+	return (0);
+}
