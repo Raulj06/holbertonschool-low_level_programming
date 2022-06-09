50d73f61917ab14fa42e3c9fe2333f5885d22612
Raulj06 <5015@holbertonschool.com>
2022-06-09 01:01:57 +0000

    fibonacci numbers

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - fibonacci sequence numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+	unsigned long int sum = 0;
+	unsigned long int prevSum1 = 0;
+	unsigned long int prevSum2 = 1;
+	unsigned long int newSum = 0;
+
+	while (sum <= 4000000)
+	{
+		sum = prevSum1 + prevSum2;
+
+		prevSum1 = prevSum2;
+		prevSum2 = sum;
+
+		if ((sum % 2) == 0)
+		{
+			newSum += sum;
+		}
+
+		n++;
+	}
+	printf("%lu\n", newSum);
+	return (0);
+}
