49cc87b639473c3cf6e88448e7d93e905526ebfb
Raulj06 <5015@holbertonschool.com>
2022-06-09 01:04:07 +0000

    100 fibonacci numbers

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints 100 fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	float fib1 = 1, fib2 = 2, fib3, num, count = 0;
+
+	num = 100;
+
+	printf("%.0f, ", fib1);
+
+	printf("%.0f, ", fib2);
+
+	count = 4;
+
+	while (count < num)
+
+	{
+		fib3 = fib1 + fib2;
+
+		count++;
+
+		printf("%.0f", fib3);
+
+		fib1 = fib2;
+
+		fib2 = fib3;
+		if (count != 100)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
