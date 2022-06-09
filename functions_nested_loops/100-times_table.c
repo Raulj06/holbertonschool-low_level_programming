394f07ca407e5200795e280392df9250e2218acd
Raulj06 <5015@holbertonschool.com>
2022-06-09 00:51:07 +0000

    n times table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * prints_times_table - print the n times table
+ *@n: test
+ * Return: Always 0.
+ */
+
+void print_times_table(int n)
+{
+
+	int num, multi, total;
+
+	if (n >= 0 && n <= 15)
+	{
+
+		for (num = 0; num <= n; num++)
+
+		{
+			_putchar('0');
+
+			for (multi = 1; multi <= n; multi++)
+			{
+				_putchar(44);
+				_putchar(32);
+
+				total = num * multi;
+
+				if (total <= 99)
+
+					_putchar(32);
+
+				if (total <= 9)
+
+					_putchar(32);
+
+				if (total >= 100)
+				{
+					_putchar((total / 100) + '0');
+					_putchar(((total / 10)) % 10 + '0');
+				}
+
+				else if (total <= 99 && total >= 10)
+				{
+					_putchar((total / 10) + '0');
+				}
+				_putchar((total % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
