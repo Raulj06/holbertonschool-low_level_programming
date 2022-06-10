745e645cb06db47829f2ace8cc7a87a8d9e32cac
Raulj06 <5015@holbertonschool.com>
2022-06-10 16:33:37 +0000

    like comment

---
+++ debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - prints if interger is positive
+ *
+ * Return: 0
+ */
+positive_or_negative(void)
+{
+
+	int 1;
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+	
+
