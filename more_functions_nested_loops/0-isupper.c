f222fde1eb319da8a86dc2251cb8490aeddea0ec
Raulj06 <5015@holbertonschool.com>
2022-06-13 14:34:31 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case character
+ *@c: test
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+
+		return (0);
+}
