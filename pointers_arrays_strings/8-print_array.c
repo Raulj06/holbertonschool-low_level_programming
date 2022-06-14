02bf69bdc81dfaf6bf8272c2701aab66d20e8cad
Raulj06 <5015@holbertonschool.com>
2022-06-14 18:30:34 +0000

    print n elements

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdio.h> 
+/**
+ * print_array - prints n element
+ *@arr: test
+ *@n: test
+ */
+
+void print_array(int *arr, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", arr[i]);
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
