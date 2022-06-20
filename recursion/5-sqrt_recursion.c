7af046d1eaf8ceeb5a4dcf2048d0abf81af18512
Raulj06 <5015@holbertonschool.com>
2022-06-20 19:50:54 +0000

    square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *_sqrt_find - find the square root
+ *@n: test
+ *@number: test
+ *Return: the square root
+ */
+
+int _sqrt_find(int n, int num)
+{
+	if (num * num == n)
+		return (num);
+
+	if (num * num > n)
+		return (-1);
+
+	return (_sqrt_find(n, num + 1));
+}
+
+
+/**
+ *_sqrt_recursion - returns the natural
+ *@n: test
+ *Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+
+	int num = 0;
+
+	if (num < 0)
+		return (-1);
+
+	if (num == 1)
+		return (1);
+
+	return (_sqrt_find(n, num));
+}
