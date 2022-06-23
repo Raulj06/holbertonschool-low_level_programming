89547d3b44f65490b303b01c9dccaf97b5d64f74
Raulj06 <5015@holbertonschool.com>
2022-06-23 19:18:53 +0000

    print its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - prints its name
+ *@argc: count
+ *@argv: test
+ *Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
