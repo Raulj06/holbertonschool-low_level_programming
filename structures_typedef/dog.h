613fbed035bdd5f615a5d2a8addb28be18014abc
Raulj06 <5015@holbertonschool.com>
2022-06-30 19:42:38 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+ *struct dog - atrubutes
+ *@name: the name
+ *@age: the age
+ *@owner: the owner
+ */
+
+struct dog
+{
+
+	char *name;
+	float *age;
+	char *owner;
+};
+/*prototypes*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
