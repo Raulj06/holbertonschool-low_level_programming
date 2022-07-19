a6b3cd4c9ad0247e3089af8602483a68eb0c05ee
Raulj06 <5015@holbertonschool.com>
2022-07-19 19:09:16 +0000

    print a number

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: a decimal number
+ * @index: index starting from 0 of the bit you want to get
+ *
+ * Return: nothing
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int idx;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	for (idx = 0; idx < index; idx++)
+		n = n >> 1;
+
+	return (n & 1);
+}
