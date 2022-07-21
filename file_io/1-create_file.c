054e18bfdfc08587cbee6bedfcae22eb4e586464
Raulj06 <5015@holbertonschool.com>
2022-07-21 17:23:43 +0000

    create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file
+ * @text_content: NULL
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, index;
+	size_t count = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 00600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+		return (-1);
+
+	index = 0;
+	while (text_content[index])
+	{
+		index++;
+		count++;
+	}
+
+	write(fd, text_content, count);
+
+	close(fd);
+	return (1);
+}
