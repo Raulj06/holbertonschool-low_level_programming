b788d60cb420479409c2de2c5f9efe3184e7e051
Raulj06 <5015@holbertonschool.com>
2022-07-22 21:26:53 +0000

    insert a new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,36 +1,51 @@
 #include "lists.h"
 
 /**
- *insert_dnodeint_at_index - will insert a new node at a given position
- *@h: first position
- *@idx: position we need to get
- *@n: new value
- *Return: return the new value
- */
-
+  * insert_dnodeint_at_index - inserts a new node
+  * @h: the first element
+  * @idx: the index of the list
+  * @n: value
+  *
+  * Return: the address of the new node, or NULL if it failed
+  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *ittr;
-	unsigned int count = 0;
+	unsigned int index = 0;
+	dlistint_t *new;
+	dlistint_t *tmp = *h;
+
+	if (h == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	ittr = *h;
-	if (ittr == NULL || count == idx)
+
+	if (idx == 0)
 	{
-		new->next = ittr;
+		new->next = *h;
+		new->prev = NULL;
+		(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
 
-	while (ittr && ittr->next && count < idx - 1)
+	while (tmp != NULL)
 	{
-		ittr = ittr->next;
-		count++;
+		if (index == idx - 1)
+		{
+			new->next = tmp->next;
+			new->next->prev = new;
+			new->prev = tmp;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
+		index++;
+		if (index > idx)
+			return (NULL);
 	}
-	new->next = ittr->next;
-	ittr->next = new;
-	return (new);
+
+	return (NULL);
 }
