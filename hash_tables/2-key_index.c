fa5d7158b29bba185ee5a72f02ed0393cfc0fdb4
Raulj06 <5015@holbertonschool.com>
2022-08-21 20:24:22 +0000

    implementing

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,8 +9,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int key = 0;
+	unsigned long int new_key = 0;
 
-	key = (hash_djb2(key) % size);
-	return (key);
+	new_key = (hash_djb2(key) % size);
+	return (new_key);
 }
