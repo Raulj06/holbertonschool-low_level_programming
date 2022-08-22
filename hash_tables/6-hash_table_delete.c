5143af2b5c72b542a9a2b1eacd439de25860688b
Raulj06 <5015@holbertonschool.com>
2022-08-22 01:55:50 +0000

    deletes a hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,41 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * free_link_list - free linked list in hash table
- * @head: beginning of linked list
- * Return: none
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
  */
-
-void free_link_list(hash_node_t *head)
+void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *pop_candidate;
-	hash_node_t *change_node;
+	unsigned long int index;
+	hash_node_t *current_node, *temp_node;
 
-	pop_candidate = head;
-	while (pop_candidate)
+	if (ht == NULL)
+		return;
+
+
+	for (index = 0; index < ht->size; index++)
 	{
-		change_node = pop_candidate->next;
-		free(pop_candidate->value);
-		free(pop_candidate->key);
-		free(pop_candidate);
-		pop_candidate = change_node;
+		if (ht->array[index] != NULL)
+		{
+			current_node = ht->array[index];
+			while (current_node != NULL)
+			{
+				temp_node = current_node->next;
+				free(current_node->value);
+				free(current_node->key);
+				free(current_node);
+				current_node = temp_node;
+			}
+		}
 	}
-}
-
-/**
- * hash_table_delete - free hash table
- * @hash: hash table to be freed
- * Return: none
- */
-void hash_table_delete(hash_table_t *hash)
-{
-	size_t idx = 0;
-
-	while (idx < hash->size)
-	{
-		free_link_list(hash->array[idx]);
-		idx++;
-	}
-	free(hash->array);
-	free(hash);
+	free(ht->array);
+	free(ht);
 }
